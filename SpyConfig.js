d80753f2825b84db4b90f53c674a651c9cba93db
Aeorq <130874582+Aeorq@users.noreply.github.com>
2023-04-26 22:15:04 +0800

    更新 SpyConfig.js

--- SpyConfig.js
+++ SpyConfig.js
@@ -764,14 +764,14 @@ let RspyList = {
     ListMode: 2,
     /* 运行日志输出位置,例如错误运行日志/任务运行成功等日志,只能设置1个 */
     runLogsInfo: {
-        platform: 'HumanTG', //发送平台
-        toGroupOrUser: 'groupId', //通知类型,个人userId //群groupId
-        Id: '-983627023', //个人id 或群id
+        platform: 'qq', //发送平台
+        toGroupOrUser: 'userId', //通知类型,个人userId //群groupId
+        Id: '528006323', //个人id 或群id
     },
     /* 静默后监控结果输出位置  可填多个*/
     TabooLogsInfo: [
         // 	{
-        // 	platform: "qqBot",
+        // 	platform: "qq",
         // 	toGroupOrUser: "userId",   //通知类型,个人userId //群groupId
         // 	Id: "528006323"
         // },

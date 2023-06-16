2a935c28d42e6c2a2707a57591399943a7c3a846
Aeorq <130874582+Aeorq@users.noreply.github.com>
2023-06-17 00:34:40 +0800

    更新 spyConfig.js

--- spyConfig.js
+++ spyConfig.js
@@ -25,3 +25,15 @@ let RspyList = {
             RunPanel: [0],
             Disable: false,
         },
+        {
+   Name: 'M-分享有礼',
+            Script: 'walle1798_WALL.E/m_jd_wx_share.js',
+            ListenEnv: ['jd_wxShareActivity_activityId', 'M_WXM_WX_SHARE_URL'],
+            SetEnv: {
+             jd_wxShareActivity_activityId: 'M_WX_SHARE_URL',
+            },
+            TimeOut: 0,
+            Interval: 0,
+            RunPanel: [0],
+            Disable: false,
+        },

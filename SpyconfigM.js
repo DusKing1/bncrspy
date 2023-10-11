8678c273e98e79fbd13c97bdc8daf569d78ef6b7
Aeorq <130874582+Aeorq@users.noreply.github.com>
2023-10-12 05:30:22 +0800

    更新 SpyconfigM.js

--- SpyconfigM.js
+++ SpyconfigM.js
@@ -3,22 +3,19 @@ const RspyList = {
     SpyList: [
         {
             /* 任务名 确保任务名唯一性,因为id根据任务名计算生成*/ 
-			Name: 'Spy测试1',
+			Name: 'M佬关注有礼',
             /* 执行的脚本名 */
-            Script: 'spy/test.js',
+            Script: 'walle1798_EVE/m_jd_follow_shop.js',
             /* 监听变量 */
-            ListenEnv: ['jdzz', 'jdhb', 'mtgh'],
+            ListenEnv: ['M_FOLLOW_SHOP_ARGV'],
             /* 转换变量 */
-            SetEnv: {
-                jdzz: 'zhuanzhuan',
-                jdhb: 'hongbao',
             },
             /* 超时退出 (秒)*/
             TimeOut: 0,
             /* 间隔时间(秒) */
             Interval: 30,
             /* 运行面板 0 代表 面板管理中的第一个容器 以此类推 */
-            RunPanel: [0, 1, 2],
+            RunPanel: [0],
             /* 禁用任务 */
             Disable: false,
         },
